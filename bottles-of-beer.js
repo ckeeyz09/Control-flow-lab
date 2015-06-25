78b5945e50da29603102ff6535b526e71d03e63f
Sean Hill <TonkaTruck@Seans-MacBook-Air.local>
2015-06-25 11:47:05 -0700

    modified the bottles of beer song

--- bottles-of-beer.js
+++ bottles-of-beer.js
@@ -1,13 +1,17 @@
 
 var x = prompt("How many bottles of beer on the wall?");
 for (i=x; i>=0; i-=1) {
-    if (i > 1) {
+    if (i > 2) {
         console.log(i + ' ' + "bottles of beer on the wall," + ' ' + i + ' '  + "bottles of beer! Take one down and pass it around," + ' ' + (i - 1) + ' ' + "bottles of beer on the wall!")
     }
+    else if (i > 1){
+     console.log(i + ' ' + "bottles of beer on the wall," + ' ' + i + ' '  + "bottles of beer! Take one down and pass it around," + ' ' + (i - 1) + ' ' + "bottle of beer on the wall!")
+    }
     else if (i > 0){
-      i + ' ' + "bottles of beer on the wall," + ' ' + i + ' '  + "bottles of beer! Take one down and pass it around," + ' ' + (i - 1) + ' ' + "bottle of beer on the wall!")
+     console.log(i + ' ' + "bottles of beer on the wall," + ' ' + i + ' '  + "bottles of beer! Take one down and pass it around,"  + ' ' + "no more bottles of beer!")
     }
     else {
         console.log('No more')
+       
     }
 }
